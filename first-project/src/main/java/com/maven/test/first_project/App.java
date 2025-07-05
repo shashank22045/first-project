564e303ea5a683acacc4869b2deb9d4efdd291a7
s43861999 <schouksey4@gmail.com>
2025-07-05 09:11:50 +0530

    added first git maven based git project

---
+++ first-project/src/main/java/com/maven/test/first_project/App.java
@@ -0,0 +1,8 @@
+package com.maven.test.first_project;
+
+public class App {
+  public static void main(String[] args) {
+    System.out.println("Hello World!");
+    
+  }
+}
---
+++ first-project/src/test/java/com/maven/test/first_project/AppTest.java
@@ -0,0 +1,13 @@
+package com.maven.test.first_project;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class AppTest {
+    
+  @Test
+  public void shouldAnswerWithTrue() {
+    assertTrue(true);
+  }
+}
